bea6f2e31b83bdb5c3baeb14b154c8d659bdba67
Your Name <caihebatista@gmail.com>
2024-06-11 00:54:02 -0300

    proj setup

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Logger } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    Logger.log(this.appService.getHello());
+    return this.appService.getHello();
+  }
+  @Get('/test')
+  getTest(): string {
+    return 'This is a test';
+  }
+}
